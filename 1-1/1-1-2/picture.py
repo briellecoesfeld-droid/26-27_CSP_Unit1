2bbc7cc6cef68f511347325018fbd411bb026ad7
BC <brielle.coesfeld@vtsd.com>
2026-09-14 14:00:49 -0400

    My happy smile picture! Almost done.

--- 1-1/1-1-2/picture.py
+++ 1-1/1-1-2/picture.py
@@ -13,6 +13,9 @@ painter.pendown()
 # draw a circle
 painter.circle(100, 360)
 
+# smiley face name
+print = input("What is the smiley faces name?")
+
 # make left eye
 painter.penup()
 painter.goto(-30, 120)
@@ -45,7 +48,21 @@ fillcolor = "blue"
 painter.fillcolor(fillcolor)
 painter.penup()
 
+# move pen to start making smile
+painter.penup()
+painter.goto(-60, 70)
+painter.pendown()
 
+#draw line for mouth
+painter.right(90)
+painter.circle(60, 180)
+
+#make nose
+painter.penup()
+painter.goto(0, 90)
+painter.right(180)
+painter.pendown()
+painter.circle(15, 90)
 
 # create screen object and make it persist
 wn = trtl.Screen()
