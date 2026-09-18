83e9ace5469e13cf6832652afb17d54ddfa313f5
BC <brielle.coesfeld@vtsd.com>
2026-09-18 12:26:07 -0400

    Python basics

---
+++ 1-1/1-1-3/PythonBasics.py
@@ -0,0 +1,22 @@
+# variables - holds data
+# name = value
+# use self commenting identifiers
+# first_name = input("What is your first name?")
+# single = means assignment
+address_1 = ("1 Apple Tree Ave")
+address_2 = ("2 Plum Tree Ave")
+city = "Vernon"
+state = "NJ"
+zip = "07462"
+
+# how do I use the data?
+# direct access to the values - just use the name!
+num_1 = 6
+num_2 = 5
+total = num_1 + num_2
+# printing values - print() function!
+# ber careful about data types!
+print(num_1)
+# concatenation - addition for strings
+print(city + " " + state)
+print(zip + " " + city + " " + state + " " + zip)
