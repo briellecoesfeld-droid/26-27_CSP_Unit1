d4bfac9a551afb944425954a82ff31c21b799495
BC <brielle.coesfeld@vtsd.com>
2026-09-25 13:56:34 -0400

    Loops

---
+++ 1-1/1.1.4/Step.5.py
@@ -0,0 +1,17 @@
+#   a114_divisible.py
+
+# get two numbers from user
+number1 = int(input("What is number 1"))
+number2 = int(input("What is number 2"))
+
+# loop while the numbers are not divisible (the remainder is not 0)
+while number1 % number2 = 0
+    # inform user of result
+    print("Those numbers are not divisible")
+
+    # gather user input again
+    number1 = int(input("Please input a number"))
+    number2 = int(input("Please input another number"))
+
+# inform user of result
+print("Those numbers are divisible")
